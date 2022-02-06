880f38f52191ffbf57bd028a351f74589dac26fb
btijerino16 <92263027+btijerino16@users.noreply.github.com>
2022-02-06 16:40:24 -0700

    saved all files with commenting

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,11 +1,24 @@
 const router = require('express').Router();
 const { Comment } = require('../../models');
+const commentRoutes = require('./comment-routes');
+
+router.use('/comments', commentRoutes
 
 router.get('/', (req, res) => {
 
 });
 
 router.post('/', (req, res) => {
+    Comment.create({
+        comment_text: req.body.comment_text,
+        user_id: req.body.user_id,
+        post_id: req.body.post_id
+      })
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+          console.log(err);
+          res.status(400).json(err);
+        });
 
 });
 
